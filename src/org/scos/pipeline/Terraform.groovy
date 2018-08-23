9048ac3b8c79d7a784e1bc275cf81a57842cafbf
paul-jacob <paul-jacob@users.noreply.github.com>
2018-08-23 13:23:46 -0400

    Smrt 340: Updates to Terraform class (#17)
    * Extract workspace list generation into private getter
    
    * Refactor plan command generation
    
    * SMRT-340 add prod prime to backend mapping in Terraform.groovy
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>
    
    * SMRT-340 replace backendsMap with almDeployments
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>
    
    * SMRT-340 terraform lan - now with customizable var files!
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>
    
    * SMRT-340 refactored plan and added destroyPlan
    
    co-authored-by: Kyle Smith <kylesmith@pillartechnology.com>
    co-authored-by: Jessie Morris <jmorris2@pillartechnology.com>

--- src/org/scos/pipeline/Terraform.groovy
+++ src/org/scos/pipeline/Terraform.groovy
@@ -2,7 +2,7 @@ package org.scos.pipeline
 
 class Terraform implements Serializable {
     def pipeline, environment
-    def backendsMap = [dev: "alm", staging: "alm", prod: "alm"]
+    def almDeployments = ['dev', 'staging', 'prod', 'prod-prime']
 
     Terraform(pipeline, environment) {
         this.pipeline = pipeline
@@ -10,24 +10,23 @@ class Terraform implements Serializable {
     }
 
     def outputsAsJson() {
-        def bucket_name = "scos-${backendsMap.get(environment, 'sandbox')}-terraform-state"
+        def bucket_name = "scos-${almDeployments.contains(environment) ? 'alm' : 'sandbox'}-terraform-state"
         def result = pipeline.sh(
             returnStdout: true,
             script: "aws s3 cp s3://${bucket_name}/env:/${environment}/operating-system -"
-            ).trim()
+        ).trim()
         pipeline.readJSON(text: result).modules[0].outputs
+    }
+
+    String getDefaultVarFile() {
+        pipeline.fileExists("variables/${environment}.tfvars") ?
+            "variables/${environment}.tfvars" :
+            "variables/sandbox.tfvars"
     }
 
     void init() {
         pipeline.sh 'rm -rf .terraform'
-        pipeline.sh "terraform init --backend-config=../backends/${backendsMap.get(environment, 'alm-sandbox')}.conf"
-
-        List workspaces = pipeline.sh(
-            returnStdout: true,
-            script: "terraform workspace list"
-            ).split('\n').collect {
-                it.substring(2)
-            }
+        pipeline.sh "terraform init --backend-config=../backends/${almDeployments.contains(environment) ? 'alm' : 'sandbox-alm'}.conf"
 
         if(workspaces.contains(environment)) {
             pipeline.sh "terraform workspace select ${environment}"
@@ -36,18 +35,18 @@ class Terraform implements Serializable {
         }
     }
 
-    void plan(Map extra_variables = [:]) {
-        def varFile = pipeline.fileExists("variables/${environment}.tfvars") ?
-            "variables/${environment}.tfvars" :
-            "variables/sandbox.tfvars"
-
+    void plan(varFile, Map extra_variables = [:], List extra_args = []) {
         String planCommand = "terraform plan "
+        planCommand += "--var-file=${varFile} "
 
         extra_variables.each { key, value ->
             planCommand += "--var=${key}='${value}' "
         }
 
-        planCommand += "--var-file=${varFile} "
+        extra_args.each { arg ->
+            planCommand += "${arg} "
+        }
+
         planCommand += "--out=${environment}.plan"
 
         def planOutput = pipeline.sh(returnStdout: true, script: planCommand)
@@ -56,7 +55,16 @@ class Terraform implements Serializable {
         pipeline.writeFile(file: "plan-${environment}.txt", text: planOutput)
     }
 
+    void planDestroy(varFile) {
+        plan(varFile, [:], ['--destroy'])
+    }
+
     void apply() {
         pipeline.sh("terraform apply ${environment}.plan")
+    }
+
+    private List getWorkspaces() {
+        def workspaceStr = pipeline.sh(script: "terraform workspace list", returnStdout: true)
+        workspaceStr.split('\n').collect { it.substring(2) }
     }
 }
